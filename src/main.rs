145adfc749128092b05cc7e998f72aeccb3ed5a9
Eric Dattore <edattore@gmail.com>
2016-07-09 09:46:59 -0600

    chore(exec): Refactor subcommand handling
    Refactored subcommand handling to use if-let bindings rather than
    matches with let-if statements. Reformatted the main file with rustfmt
    as well.

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,24 @@ const VERSION_NUMBER: &'static str = "0.1.0";
 
 fn main() {
     let matches = App::new("Rukyll")
-                        .setting(AppSettings::ArgRequiredElseHelp)
-                        .version(VERSION_NUMBER)
-                        .about("Static site generator")
-                        .subcommand(SubCommand::with_name("new")
-                            .arg(Arg::with_name("project name")
-                            .index(1)
-                            .required(true)
-                            )
-                        )
-                        .get_matches();
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .version(VERSION_NUMBER)
+        .about("Static site generator")
+        .subcommand(SubCommand::with_name("new").arg(Arg::with_name("project name")
+            .index(1)
+            .required(true)))
+        .subcommand(SubCommand::with_name("build"))
+        .get_matches();
 
-    match matches.subcommand() {
-        ("new", Some(new)) => {
-            // TODO: Better error handling
-            let project_name = if new.is_present("project name") {
-                new.value_of("project name").unwrap()
-            } else {
-                ""
-            };
 
-            commands::new_project(&project_name);
-        },
-        _ => {}
+    if let ("new", Some(new)) = matches.subcommand() {
+        // TODO: Better error handling
+        let project_name = if new.is_present("project name") {
+            new.value_of("project name").unwrap()
+        } else {
+            ""
+        };
+
+        commands::new_project(&project_name);
     }
 }
