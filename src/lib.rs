1696b275320da7165f371d6fd4ea896cbf6c7189
Eric Dattore <edattore@gmail.com>
2016-07-22 13:25:45 -0600

    Revert "chore: cleanup"
    This reverts commit d3c55fbb5180e62b77edb08987cc8fd69e1bcc86.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 extern crate pulldown_cmark;
 extern crate yaml_rust;
 
+// TODO: Remove this once the integration tests are extracted out
+#[cfg(test)]
+extern crate tempdir;
+
 pub mod commands;
 pub mod config;
 pub mod generation;
