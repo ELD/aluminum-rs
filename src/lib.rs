d3c55fbb5180e62b77edb08987cc8fd69e1bcc86
Eric Dattore <edattore@gmail.com>
2016-07-22 07:24:50 -0600

    chore: cleanup
    [ci skip]

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
 extern crate pulldown_cmark;
 extern crate yaml_rust;
 
-// TODO: Remove this once the integration tests are extracted out
-#[cfg(test)]
-extern crate tempdir;
-
 pub mod commands;
 pub mod config;
 pub mod generation;
