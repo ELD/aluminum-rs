8a8d5c2a79b4fea636d55e2b3b00af6d3ec8748f
Eric Dattore <eric.dattore@gmail.com>
2017-02-23 21:57:31 -0700

    [#140401527] thread sleep instead of retry (#13)

--- ci/check_code_coverage.sh
+++ ci/check_code_coverage.sh
@@ -1,18 +1,22 @@
 if [[ $TRAVIS_RUST_VERSION = 'stable' ]]; then
     # FIXME: Dirty hack to get kcov working for now...
-    wget https://github.com/SimonKagstrom/kcov/archive/1e13fa40156c6717d1efd1f1c13fee8a94b8eea0.zip
-    unzip 1e13fa40156c6717d1efd1f1c13fee8a94b8eea0.zip
-    mv kcov-1e13fa40156c6717d1efd1f1c13fee8a94b8eea0 kcov-master
-    cd kcov-master
+    wget https://github.com/SimonKagstrom/kcov/archive/v33.zip
+    unzip v33.zip
+    cd kcov-33
     mkdir build
     cd build
     cmake ..
     make
-    make install DESTDIR=../tmp
+    make install DESTDIR=../../tmp
     cd ../..
+    rm -rf kcov-33
+
     for file in target/debug/aluminum-*; do
-        ./kcov-master/tmp/usr/local/bin/kcov --exclude-pattern=/.cargo,target/ --verify target/kcov target/debug/aluminum-*
+        mkdir -p "target/cov/$(basename $file)"
+        tmp/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib,tests/ --verify "target/cov/$(basename $file)" "$file"
     done
 
-    ./kcov-master/tmp/usr/local/bin/kcov --coveralls-id=$TRAVIS_JOB_ID --exclude-pattern=/.cargo,target/ --verify target/kcov target/debug/lib-*
+    tmp/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib,tests/ --verify target/cov/lib target/debug/lib-*
+
+    bash <(curl -s https://codecov.io/bash)
 fi
